55d9a666260ab58f4abe035763acc5f6269360b0
Akshay Uppal <akshayuppal3@gmail.com>
2018-12-02 15:41:38 -0600

    resolving cached files

--- src/data_extraction/authentication.py
+++ src/data_extraction/authentication.py
@@ -7,13 +7,13 @@
 import requests
 class Authenticate:
 	def __init__(self):
-		self.consumer_key = '#'
-		self.consumer_secret = '#'
-		self.access_token = "#"
-		self.access_secret = '#'
+		self.consumer_key = 'bV031fLEksoIvV6gO3juVe5fw'
+		self.consumer_secret = '3xjNTYfKvpgkkWRbi4blyC8GGVKuBjUPMGLXpab0w9IFiJ6ITv'
+		self.access_token = "141309128-bDGWx9p1g6OS0uJZm945YVMetRKg9o6T9U2xv8uf"
+		self.access_secret = 'SdP13GF6LZxbmB6bqkQpiKUdaZROvQxQDD9S8Kgb0lwHw'
 		# crimson credentials
-		self.username = "#"
-		self.password = "#"
+		self.username = "auppal8@uic.edu"
+		self.password = "Xperia123@"
 
 	def getConsumerKey(self):
 		return(self.consumer_key)
