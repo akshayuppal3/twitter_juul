09004e05dd975e1bf428e93b998218fd2b6506ce
Akshay Uppal <akshayuppal3@gmail.com>
2018-12-01 18:03:38 -0600

    minor changes

--- src/data extraction/following_data.py
+++ src/data extraction/following_data.py
@@ -183,8 +183,7 @@ if __name__ == '__main__':
         filename_input = args['inputFile3']
         filename_output = args['outputFile3']
         filename_output = os.path.join(util.inputdir, filename_output + '.csv')
-        dtypes = {'userID': 'int', 'following': 'str'}
-        df = pd.read_csv(filename_input, dtype=dtypes)
+        df = util.read_excel(filename_input)
         if df is not None:
             ## getting the list of userIDs
             if 'userID' in df:
