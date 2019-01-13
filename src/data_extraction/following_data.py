60357a2b8d6ca637565cfe149e30a5526a040526
Akshay Uppal <akshayuppal3@gmail.com>
2019-01-13 17:42:37 -0600

    minor change

--- src/data_extraction/following_data.py
+++ src/data_extraction/following_data.py
@@ -59,7 +59,7 @@ class twitter_following():
 			api = apis[0]
 			userData = self.getUserTimelineData(user,api)
 			if ((idx > 0) & (idx % 2 == 0)):  # dump after every 500
-				filename = filename_output+ str(count) + '.csv'
+				filename = filename_output+ str(count) + '.csv' + '.pkl'
 				with open(filename,"wb") as f:
 					pickle.dump(finalData,f)
 				count += 1
